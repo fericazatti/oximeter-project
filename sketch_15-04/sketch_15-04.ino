3998aa183101554117ea35f3a5ae34e13e9597cf
Fernando <fericazatti@gmail.com>
2021-05-14 16:19:55 -0300

    Timmer de .5 segundos y conexion bluetooth

--- sketch_15-04/sketch_15-04.ino
+++ sketch_15-04/sketch_15-04.ino
@@ -1,16 +1,51 @@
+#include "BluetoothSerial.h"
+
 #define RXD2 39
 #define TXD2 17
 #define led 2
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+
+BluetoothSerial SerialBT;
+boolean confirmRequestPending = true;
 byte pack[5], imenu=1;
 const byte push1 = 23;
+boolean segundo=false;
 
-void setup() {
-  Serial.begin(115200);
-  pinMode (led, OUTPUT);
-  pinMode(push1, INPUT_PULLUP);
-  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
-  delay(100);
-  attachInterrupt(digitalPinToInterrupt(push1) , menu, FALLING);
+volatile int interruptCounter;
+int totalInterruptCounter;
+boolean  estadoled=false;
+
+hw_timer_t * timer = NULL;
+portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+
+void BTConfirmRequestCallback(uint32_t numVal)
+{
+  confirmRequestPending = true;
+  Serial.println(numVal);
+}
+
+void BTAuthCompleteCallback(boolean success)
+{
+  confirmRequestPending = false;
+  if (success)
+  {
+    Serial.println("Pairing success!!");
+  }
+  else
+  {
+    Serial.println("Pairing failed, rejected by user!!");
+  }
+}
+
+void IRAM_ATTR onTimer() {
+  portENTER_CRITICAL_ISR(&timerMux);
+  interruptCounter++;
+  portEXIT_CRITICAL_ISR(&timerMux);
+ 
 }
 
 byte verificar() {
@@ -50,15 +85,65 @@ void UpdateData()  {
   case 3: 
     Serial.println(pack[2]);
     break;
-  case 4: 
-    Serial.println(pack[3]);
-    break;   
-  case 5: 
-    Serial.println(pack[4]); 
-    break;
+  }
+  if (segundo)
+  {
+    switch (imenu)
+    {
+    case 4: 
+      Serial.println(pack[3]);
+      SerialBT.write(pack[3]);
+      break;   
+    case 5: 
+      Serial.println(pack[4]); 
+      SerialBT.write(pack[4]);
+
+      break;
+    }
+    segundo = false;
   }
 } 
 
+void setup() {
+  Serial.begin(115200);
+  pinMode (led, OUTPUT);
+  pinMode(push1, INPUT_PULLUP);
+  Serial2.begin(115200, SERIAL_8N1, RXD2, TXD2);
+  delay(100);
+  attachInterrupt(digitalPinToInterrupt(push1) , menu, FALLING);
+  //setup timmer
+  timer = timerBegin(0, 80, true);
+  timerAttachInterrupt(timer, &onTimer, true);
+  timerAlarmWrite(timer, 500000, true);
+  timerAlarmEnable(timer);
+  //setup bluethooth
+  Serial.begin(115200);
+  SerialBT.enableSSP();
+  SerialBT.onConfirmRequest(BTConfirmRequestCallback);
+  SerialBT.onAuthComplete(BTAuthCompleteCallback);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+  
+}
+
+void confirmBT()
+{
+    if (confirmRequestPending)
+  {
+    if (Serial.available())
+    {
+      int dat = Serial.read();
+      if (dat == 'Y' || dat == 'y')
+      {
+        SerialBT.confirmReply(true);
+      }
+      else
+      {
+        SerialBT.confirmReply(false);
+      }
+    }
+  }
+}
 
 void loop() { 
   lectura();
@@ -67,5 +152,14 @@ void loop() {
     byte a = Serial2.read();
     pack[0]=143;
   }
+  if (interruptCounter > 0) 
+  {
+    portENTER_CRITICAL(&timerMux);
+    interruptCounter--;
+    portEXIT_CRITICAL(&timerMux);
+    segundo = true;
+    totalInterruptCounter++;
+  }
+  confirmBT();
   delay(1);
 }
